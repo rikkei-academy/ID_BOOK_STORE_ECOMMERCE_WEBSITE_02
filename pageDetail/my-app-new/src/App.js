892baba52e3c7cade59e63b3f346aff70f893596
LeDuc2004 <114064159+LeDuc2004@users.noreply.github.com>
2023-03-13 12:00:54 +0700

    Add files via upload

---
+++ pageDetail/my-app-new/src/App.js
@@ -0,0 +1,16 @@
+import "./components/detail.css"
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Detail from "./components/Detail";
+
+function App() {
+  return ( 
+    <>
+    <Header></Header>
+    <Detail></Detail>
+    <Footer></Footer>
+    </>
+   );
+}
+
+export default App;
---
+++ pageDetail/my-app-new/src/components/Catalog.jsx
@@ -0,0 +1,249 @@
+function Detail() {
+    return (  
+<>
+  <div className="detail">
+    <div className="detail-img">
+      <img src="" alt="" />
+    </div>
+    <div className="detail-ttsp">
+      <div className="ttsp-name">Think and Grow Rich</div>
+      <div className="ttsp-star-sosal">
+        <div className="ttsp-list-star ng">
+          <i className="fa-solid fa-star" />
+          <i className="fa-solid fa-star" />
+          <i className="fa-solid fa-star" />
+          <i className="fa-solid fa-star" />
+          <i className="fa-solid fa-star" />
+          <div>4.0</div>
+        </div>
+        <div className="ttsp-list-sosal">
+          <div className="ttsp-reviems ng">
+            <img src="imgdetail/chat (2) 1.png" alt="" />
+            <div className="ttsp-text"> 235 Reviews</div>
+          </div>
+          <div className="ttsp-reviems ng">
+            <img src="imgdetail/thumb-up 1.png" alt="" />
+            <div className="ttsp-text">456k Like</div>
+          </div>
+          <div className="ttsp-list-4">
+            <img src="imgdetail/social01 (1).png" alt="" />
+            <img src="imgdetail/social03 (1).png" alt="" />
+            <img src="imgdetail/social05 (1).png" alt="" />
+            <img src="imgdetail/social05 (2).png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="list-tt-tacgia">
+        <div className="list-tt-left">
+          <div className="list-tt-left-1 ng">
+            <div className="list-tt-left-1__img">
+              <img src="" alt="" />
+            </div>
+            <div className="nd1">
+              <div className="nd__nho">Writen by</div>
+              <div className="nd__lon">Napoleon Rich</div>
+            </div>
+          </div>
+          <div className="list-tt-left-2 ng">
+            <div className="nd1">
+              <div className="nd__nho">Publisher</div>
+              <div className="nd__lon">Printarea Studios</div>
+            </div>
+          </div>
+          <div className="list-tt-left-3 ng">
+            <div className="nd1">
+              <div className="nd__nho">Year</div>
+              <div className="nd__lon">2019</div>
+            </div>
+          </div>
+        </div>
+        <div className="list-tt-right">
+          <div className="list-tt-right-1 ng">
+            <img src="imgdetail/like.png" alt="" />
+          </div>
+          <div className="list-tt-right2 ng">
+            <img src="imgdetail/like (1).png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="doan-text-dai">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div className="doan-text-dai">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div className="detail-price">
+        <div className="detail-price__left">$15,63</div>
+        <div className="detail-price__right ng">
+          <div className="btn-price ng">
+            <div className="btn-price__down">
+              <img src="imgdetail/-.png" alt="" />
+            </div>
+            <div>1</div>
+            <div className="btn-price__up">
+              <img src="imgdetail/+.png" alt="" />
+            </div>
+          </div>
+          <div className="detail-price__right-add">Add to cart</div>
+          <div className="traitim">
+            <i className="fa-regular fa-heart" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="evaluate">
+    <div className="evaluate__left">
+      <div className="evaluate__ltop">
+        <div className="evaluate__detail-product">Details Product</div>
+        <div className="evaluate__detail-review">Customer Reviews</div>
+      </div>
+      <div className="wrapp-sample">
+        <div className="sample">
+          <div className="sample__1">Book Title</div>
+          <div className="sample__2">Think and Grow Rich</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">Author</div>
+          <div className="sample__2">Napoleon Rich</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">ISBN</div>
+          <div className="sample__2">121341381648 (ISBN13: 121341381648)</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">Ediiton Language</div>
+          <div className="sample__2">English</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">Book Format</div>
+          <div className="sample__2">Paperback, 450 Pages</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">Date Published</div>
+          <div className="sample__2">August 10th 2019</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">Publisher</div>
+          <div className="sample__2">Publisher</div>
+        </div>
+        <div className="sample">
+          <div className="sample__1">Tags</div>
+          <div className="sample__2">
+            <div className="flexuong">
+              <div className="flexuong__sun">Drama</div>
+              <div className="flexuong__sun">Advanture</div>
+              <div className="flexuong__sun">Survival</div>
+              <div className="flexuong__sun">Biography</div>
+              <div className="flexuong__sun">Trending2020</div>
+              <div className="flexuong__sun">Bestseller</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="evaluate__right">
+      <div className="evaluate__detail-product">Related Books</div>
+      <div className="spnho">
+        <div className="spnho__img">
+          <img src="" alt="" />
+        </div>
+        <div className="spnho__ttsp">
+          <div className="spnho__textto">Terrible Madness</div>
+          <div className="spnho__textvang">THRILLE, DRAMA, HORROR</div>
+          <div className="spnho__price">$45.4</div>
+          <div className="spnho__btn">
+            <img src="imgdetail/cart 1 (1).png" alt="" />
+            <div className="spnho__addtocart">Add to cart</div>
+          </div>
+        </div>
+      </div>
+      <div className="spnho">
+        <div className="spnho__img">
+          <img src="" alt="" />
+        </div>
+        <div className="spnho__ttsp">
+          <div className="spnho__textto">Terrible Madness</div>
+          <div className="spnho__textvang">THRILLE, DRAMA, HORROR</div>
+          <div className="spnho__price">$45.4</div>
+          <div className="spnho__btn">
+            <img src="imgdetail/cart 1 (1).png" alt="" />
+            <div className="spnho__addtocart">Add to cart</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="testimonials">Testimonials</div>
+  <div className="lorem">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua
+  </div>
+  <div className="ng">
+    <div className="wrap-danhgia">
+      <div className="ttsp-list-star ng">
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+      </div>
+      <div className="saodep__text">
+        Very impresive store. Your book made studying for the ABC certification
+        exams a breeze. Thank you very much
+      </div>
+    </div>
+    <div className="wrap-danhgia">
+      <div className="ttsp-list-star ng">
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+      </div>
+      <div className="saodep__text">
+        Very impresive store. Your book made studying for the ABC certification
+        exams a breeze. Thank you very much
+      </div>
+    </div>
+    <div className="wrap-danhgia">
+      <div className="ttsp-list-star ng">
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+      </div>
+      <div className="saodep__text">
+        Very impresive store. Your book made studying for the ABC certification
+        exams a breeze. Thank you very much
+      </div>
+    </div>
+    <div className="wrap-danhgia">
+      <div className="ttsp-list-star ng">
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+      </div>
+      <div className="saodep__text">
+        Very impresive store. Your book made studying for the ABC certification
+        exams a breeze. Thank you very much
+      </div>
+    </div>
+  </div>
+</>
+
+    );
+}
+
+export default Detail;
